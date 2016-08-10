fe8448ae0d6442113da97e24b75b1d6a32dadbad
Clauderic Demers <me@ced.io>
2016-08-09 23:37:34 -0400

    Linting

--- src/SortableContainer/index.js
+++ src/SortableContainer/index.js
@@ -24,7 +24,7 @@ export default function SortableContainer(WrappedComponent, config = {withRef: f
 			pressDelay: 0,
 			useWindowAsScrollContainer: false,
 			hideSortableGhost: true,
-			contentWindow: typeof window !== 'undefined' ? window : null,
+			contentWindow: (typeof window !== 'undefined') ? window : null,
 			lockToContainerEdges: false,
 			lockOffset: '50%'
 		};
