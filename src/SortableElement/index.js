0077f0b4e3b50f68c04672e78b6b69b8dc880d96
Clauderic Demers <me@ced.io>
2017-04-12 07:02:58 -0400

    nit: linting tweaks

--- src/SortableElement/index.js
+++ src/SortableElement/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {findDOMNode} from 'react-dom';
 import invariant from 'invariant';
