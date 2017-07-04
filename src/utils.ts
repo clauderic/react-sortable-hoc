070943d2fbaf867080c855b153ad407750faa231
ricokahler <ricokahler@me.com>
2017-07-04 00:22:31 -0400

    convert utils to typescript

---
+++ src/utils.ts
@@ -0,0 +1,84 @@
+import * as React from 'react';
+
+export function arrayMove(arr: any[], previousIndex: number, newIndex: number) {
+  const array = arr.slice(0);
+  if (newIndex >= array.length) {
+    let k = newIndex - array.length;
+    while (k-- + 1) {
+      array.push(undefined);
+    }
+  }
+  array.splice(newIndex, 0, array.splice(previousIndex, 1)[0]);
+  return array;
+}
+
+export function omit(obj: {[key: string]: any}, ...keysToOmit: string[]) {
+  return Object.keys(obj).reduce((acc, key) => {
+    if (keysToOmit.indexOf(key) === -1) acc[key] = obj[key];
+    return acc;
+  }, {} as {[key: string]: any});
+}
+
+export const events = {
+  start: ['touchstart', 'mousedown'],
+  move: ['touchmove', 'mousemove'],
+  end: ['touchend', 'touchcancel', 'mouseup'],
+};
+
+export const vendorPrefix: string = (function () {
+  if (typeof window === 'undefined' || typeof document === 'undefined') return ''; // server environment
+  // fix for:
+  //    https://bugzilla.mozilla.org/show_bug.cgi?id=548397
+  //    window.getComputedStyle() returns null inside an iframe with display: none
+  // in this case return an array with a fake mozilla style in it.
+  const styles = window.getComputedStyle(document.documentElement, '') || ['-moz-hidden-iframe'];
+  const pre = (Array.prototype.slice.call(styles).join('').match(/-(moz|webkit|ms)-/) || ((styles as any).OLink === '' && ['', 'o']))[1];
+
+  switch (pre) {
+    case 'ms':
+      return 'ms';
+    default:
+      return pre && pre.length ? pre[0].toUpperCase() + pre.substr(1) : '';
+  }
+})();
+
+export function closest(el: Node | undefined | null, fn: (t: Node) => boolean) {
+  while (el) {
+    if (fn(el)) return el;
+    el = el.parentNode;
+  }
+}
+
+export function limit(min: number, max: number, value: number) {
+  if (value < min) {
+    return min;
+  }
+  if (value > max) {
+    return max;
+  }
+  return value;
+}
+
+function getCSSPixelValue(stringValue: string | undefined | null) {
+  if (stringValue && stringValue.substr(-2) === 'px') {
+    return parseFloat(stringValue);
+  }
+  return 0;
+}
+
+export function getElementMargin(element: HTMLElement) {
+  const style = window.getComputedStyle(element);
+
+  return {
+    top: getCSSPixelValue(style.marginTop),
+    right: getCSSPixelValue(style.marginRight),
+    bottom: getCSSPixelValue(style.marginBottom),
+    left: getCSSPixelValue(style.marginLeft),
+  };
+}
+
+export function provideDisplayName(prefix: string, Component: React.ComponentClass) {
+  const componentName = Component.displayName || Component.name;
+
+  return componentName ? `${prefix}(${componentName})` : prefix;
+}
