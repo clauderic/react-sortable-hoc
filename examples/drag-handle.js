ab05126c55c5b4e61b641f4052ee6c4db27dc57f
Matija Marohnić <matija.marohnic@gmail.com>
2017-09-06 11:33:01 +0200

    Apply ESLint autofix to examples

--- examples/drag-handle.js
+++ examples/drag-handle.js
@@ -33,14 +33,14 @@ class SortableComponent extends Component {
     items: ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6'],
   };
   onSortEnd = ({oldIndex, newIndex}) => {
-    let {items} = this.state;
+    const {items} = this.state;
 
     this.setState({
       items: arrayMove(items, oldIndex, newIndex),
     });
   };
   render() {
-    let {items} = this.state;
+    const {items} = this.state;
 
     return <SortableList items={items} onSortEnd={this.onSortEnd} useDragHandle={true} />;
   }
