c2ce3a80fdb60f13b93472fdfd3096a3acef278e
Claudéric Demers <me@ced.io>
2019-01-10 10:09:14 -0500

    docs: update examples and add collections example (#481)

--- examples/basic.js
+++ examples/basic.js
@@ -1,32 +1,36 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
-import {SortableContainer, SortableElement} from 'react-sortable-hoc';
+import {sortableContainer, sortableElement} from 'react-sortable-hoc';
 import {arrayMove} from 'array-move';
 
-const SortableItem = SortableElement(({value}) => <li>{value}</li>);
+const SortableItem = sortableElement(({value}) => <li>{value}</li>);
 
-const SortableList = SortableContainer(({items}) => {
-  return (
-    <ul>
-      {items.map((value, index) => (
-        <SortableItem key={`item-${index}`} index={index} value={value} />
-      ))}
-    </ul>
-  );
+const SortableContainer = sortableContainer(({children}) => {
+  return <ul>{children}</ul>;
 });
 
-class SortableComponent extends Component {
+class App extends Component {
   state = {
     items: ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6'],
   };
+
   onSortEnd = ({oldIndex, newIndex}) => {
-    this.setState({
-      items: arrayMove(this.state.items, oldIndex, newIndex),
-    });
+    this.setState(({items}) => ({
+      items: arrayMove(items, oldIndex, newIndex),
+    }));
   };
+
   render() {
-    return <SortableList items={this.state.items} onSortEnd={this.onSortEnd} />;
+    const {items} = this.state;
+
+    return (
+      <SortableContainer onSortEnd={this.onSortEnd}>
+        {items.map((value, index) => (
+          <SortableItem key={`item-${index}`} index={index} value={value} />
+        ))}
+      </SortableContainer>
+    );
   }
 }
 
-render(<SortableComponent />, document.getElementById('root'));
+render(<App />, document.getElementById('root'));
