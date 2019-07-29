f3679977ee993711221fc372bbbb0d437ffbf394
Benjamin Adam <benjamin.mark.adam@gmail.com>
2019-07-29 11:34:05 -0700

    Update src/SortableContainer/index.js
    Remove the includes syntax in favor of logical operators
    
    Co-Authored-By: Claudéric Demers <me@ced.io>

--- src/SortableContainer/index.js
+++ src/SortableContainer/index.js
@@ -153,7 +153,11 @@ export default function sortableContainer(
          * prevent subsequent 'mousemove' events from being fired
          * (see https://github.com/clauderic/react-sortable-hoc/issues/118)
          */
-        if (!isTouchEvent(event) && [NodeType.Anchor, NodeType.Button].includes(event.target.tagName)) {
+        if (
+          !isTouchEvent(event) &&
+          (event.target.tagName === NodeType.Anchor ||
+            event.target.tagName === NodeType.Button)
+        ) {
           event.preventDefault();
         }
 
